7332c56bbf0bd10cd224d2cef8c6c8e395b2daf1
Cyberl0rd81 <weyerer@gmx.net>
2026-03-30 15:06:21 +0200

    Update astro.config.mjs

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -2,6 +2,11 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
 export default defineConfig({
+  
+  // GitHub Pages Projekt-URL: https://test.github.io/Wikitest/
+  site: 'https://Cyberl0rd81.github.io',
+  base: '/Wikitest',
+
   integrations: [
     starlight({
       title: 'My Docs',
